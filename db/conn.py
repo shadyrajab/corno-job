15dead88f7a873221cc9382509191f5b3cf16112
shadyrajaab <shadyrajaab@gmail.com>
2024-08-15 11:13:35 -0300

    feat: adicionar admin connection

--- db/conn.py
+++ db/conn.py
@@ -1,8 +1,15 @@
 import psycopg2 
 from utils.vars import DBNAME, USER, PASSWORD, HOST
 
-CONNECTION = psycopg2.connect(
-  dbname=DBNAME,
+PLAYERS_CONNECTION = psycopg2.connect(
+  dbname='admin-players-dev',
+  user=USER,
+  password=PASSWORD,
+  host=HOST,
+)
+
+ADMIN_CONNECTION = psycopg2.connect(
+  dbname='admindb-dev',
   user=USER,
   password=PASSWORD,
   host=HOST,
