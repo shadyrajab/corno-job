0792e48dda14006919f17f7487cb2b8f2cb7d26d
shadyrajaab <shadyrajaab@gmail.com>
2024-08-14 23:59:41 -0300

    feat: adicionar inserção de hotel

--- db/funcs.py
+++ db/funcs.py
@@ -1,9 +1,14 @@
 from .conn import CONNECTION
 
+
 def get_player_users_ids(external_id, player_company_id):
     cursor = CONNECTION.cursor()
-    cursor.execute("SELECT id FROM player_users where external_id = %s and player_company_id = %s", (external_id, player_company_id))
+    cursor.execute(
+        "SELECT id FROM player_users where external_id = %s and player_company_id = %s",
+        (external_id, player_company_id),
+    )
     return cursor.fetchone()[0]
+
 
 def get_check_external_id(num_viagem):
     cursor = CONNECTION.cursor()
@@ -13,11 +18,46 @@ def get_check_external_id(num_viagem):
     if result:
         return True
 
+
+def insert_hotel(hotel):
+    cursor = None
+    try:
+        cursor = CONNECTION.cursor()
+        cursor.execute(
+            """
+            INSERT INTO hotels (
+                       id, 
+                       address_id, 
+                       name, 
+                       )
+            VALUES (default, %s, %s)
+            RETURNING id
+        """,
+            (
+                hotel["address_id"],
+                hotel["name"],
+            ),
+        )
+
+        hotel = cursor.fetchone()[0]
+        CONNECTION.commit()
+        return hotel
+
+    except Exception as e:
+        if CONNECTION:
+            CONNECTION.rollback()
+        print("Erro ao inserir hotel:", e)
+    finally:
+        if cursor:
+            cursor.close()
+
+
 def insert_flight(flight):
     cursor = None
     try:
         cursor = CONNECTION.cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             INSERT INTO flights (
                        id, 
                        departure_airport_address_id, 
@@ -42,26 +82,27 @@ def insert_flight(flight):
             VALUES (default, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             RETURNING id
         """,
-        (
-            flight['departure_airport_address_id'],
-            flight['arrived_airport_address_id'],
-            flight['departure_airport_name'],
-            flight['departure_airport_code'],
-            flight['departure_at'],
-            flight['arrived_airport_name'],
-            flight['arrived_airport_code'],
-            flight['arrived_at'],
-            flight['trip_eta'],
-            flight['external_id'],
-            flight['status'],
-            flight['airline_code'],
-            flight['airline_display_name'],
-            flight['sequence_flight'],
-            flight['flights_trip_id'],
-            flight['flight_number'],
-            flight['booking_number'],
-            flight['airline_id']
-        ))
+            (
+                flight["departure_airport_address_id"],
+                flight["arrived_airport_address_id"],
+                flight["departure_airport_name"],
+                flight["departure_airport_code"],
+                flight["departure_at"],
+                flight["arrived_airport_name"],
+                flight["arrived_airport_code"],
+                flight["arrived_at"],
+                flight["trip_eta"],
+                flight["external_id"],
+                flight["status"],
+                flight["airline_code"],
+                flight["airline_display_name"],
+                flight["sequence_flight"],
+                flight["flights_trip_id"],
+                flight["flight_number"],
+                flight["booking_number"],
+                flight["airline_id"],
+            ),
+        )
 
         flight_id = cursor.fetchone()[0]
         CONNECTION.commit()
@@ -75,25 +116,28 @@ def insert_flight(flight):
         if cursor:
             cursor.close()
 
+
 def insert_flight_trip(flight_trip):
     cursor = None
     try:
         cursor = CONNECTION.cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             INSERT INTO flights_trip (id, direction, departure_at, arrived_at, trip_id)
             VALUES (default, %s, %s, %s, %s)
             RETURNING id
         """,
-        (
-            flight_trip['direction'],
-            flight_trip['departure_at'],
-            flight_trip['arrived_at'],
-            flight_trip['trip_id'],
-        ))
+            (
+                flight_trip["direction"],
+                flight_trip["departure_at"],
+                flight_trip["arrived_at"],
+                flight_trip["trip_id"],
+            ),
+        )
 
         flight_trip_id = cursor.fetchone()[0]
         CONNECTION.commit()
-        return flight_trip_id   
+        return flight_trip_id
 
     except Exception as e:
         if CONNECTION:
@@ -104,22 +148,25 @@ def insert_flight_trip(flight_trip):
         if cursor:
             cursor.close()
 
+
 def insert_expense(expense):
     cursor = None
     try:
         cursor = CONNECTION.cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             INSERT INTO expenses (id, expenses_trip_id, currency_code, code, description)
             VALUES (default, %s, %s, %s)
             RETURNING id
         """,
-        (
-            expense['expenses_trip_id'],
-            int(float(expense['total'])),
-            expense['currency_code'],
-            expense['code'], 
-            expense['description'], 
-        ))
+            (
+                expense["expenses_trip_id"],
+                int(float(expense["total"])),
+                expense["currency_code"],
+                expense["code"],
+                expense["description"],
+            ),
+        )
 
         expense_id = cursor.fetchone()[0]
         CONNECTION.commit()
@@ -137,17 +184,19 @@ def insert_expense_trip(expense_trip):
     cursor = None
     try:
         cursor = CONNECTION.cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             INSERT INTO expenses_trip (id, total, refund_at, currency_code, trip_id)
             VALUES (default, %s, %s, %s, %s)
             RETURNING id
         """,
-        (
-            int(float(expense_trip['total'])),
-            expense_trip['refund_at'], 
-            expense_trip['currency_code'], 
-            expense_trip['trip_id']
-        ))
+            (
+                int(float(expense_trip["total"])),
+                expense_trip["refund_at"],
+                expense_trip["currency_code"],
+                expense_trip["trip_id"],
+            ),
+        )
 
         expense_id = cursor.fetchone()[0]
         CONNECTION.commit()
@@ -162,22 +211,25 @@ def insert_expense_trip(expense_trip):
         if cursor:
             cursor.close()
 
+
 def insert_hotel_trip(hotel_trip):
     cursor = None
     try:
         cursor = CONNECTION.cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             INSERT INTO hotels_trip (id, checkin_at, checkout_at, amount, currency_code, trip_id)
             VALUES (default, %s, %s, %s, %s, %s)
             RETURNING id
         """,
-        (
-            hotel_trip['checkin_at'], 
-            hotel_trip['checkout_at'], 
-            int(float(hotel_trip['amount'])),
-            hotel_trip['currency_code'], 
-            hotel_trip['trip_id']
-        ))
+            (
+                hotel_trip["checkin_at"],
+                hotel_trip["checkout_at"],
+                int(float(hotel_trip["amount"])),
+                hotel_trip["currency_code"],
+                hotel_trip["trip_id"],
+            ),
+        )
 
         hotel_id = cursor.fetchone()[0]
         CONNECTION.commit()
@@ -197,26 +249,28 @@ def insert_trip(trip):
     cursor = None
     try:
         cursor = CONNECTION.cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             INSERT INTO trip (id, start_at, end_at, status, reason, amount, currency_code, player_user_id, external_id) 
             VALUES (default, %s, %s, %s, %s, %s, %s, %s, %s) 
             RETURNING id
-        """, 
-        (
-            trip['start_at'], 
-            trip['end_at'], 
-            trip['status'], 
-            trip['reason'], 
-            trip['amount'], 
-            trip['currency_code'], 
-            trip['player_user_id'],
-            trip["external_id"]
-        ))
-        
+        """,
+            (
+                trip["start_at"],
+                trip["end_at"],
+                trip["status"],
+                trip["reason"],
+                trip["amount"],
+                trip["currency_code"],
+                trip["player_user_id"],
+                trip["external_id"],
+            ),
+        )
+
         trip_id = cursor.fetchone()[0]
-        
+
         CONNECTION.commit()
-        
+
         return trip_id
 
     except Exception as e:
@@ -229,11 +283,12 @@ def insert_trip(trip):
         if cursor:
             cursor.close()
 
+
 def get_address(address):
     cursor = CONNECTION.cursor()
     cursor.execute("SELECT id FROM addresses where address ILIKE(%s)", (address,))
     result = cursor.fetchone()
     if result:
         return result[0]
-    
+
     return
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from db.funcs import get_player_users_ids, insert_trip, insert_hotel_trip, insert_expense, insert_expense_trip, insert_flight_trip, insert_flight, get_check_external_id
+from db.funcs import (
+    get_player_users_ids,
+    insert_trip,
+    insert_hotel_trip,
+    insert_expense,
+    insert_expense_trip,
+    insert_flight_trip,
+    insert_flight,
+    get_check_external_id,
+    insert_hotel,
+)
 from utils.vars import USERS
 from sap.funcs import SAP
 
@@ -8,22 +18,23 @@ sap = SAP()
 for name, external_id in USERS.items():
     player_user_id = get_player_users_ids(external_id, 147)
     full_trip = sap.get_full_trip(name, external_id, player_user_id)
-    for trip in full_trip['trips']:
+    for trip in full_trip["trips"]:
         trip_id = insert_trip(trip)
-        
-        if get_check_external_id(trip['external_id']):
+
+        if get_check_external_id(trip["external_id"]):
             print(f"já existe uma viagem com esse ID")
             continue
 
-        hoteis = trip['hoteis']
-        expenses_trip = trip['custos_viagem']
+        hoteis = trip["hoteis"]
+        expenses_trip = trip["custos_viagem"]
 
         for hotel in hoteis:
-            hotels_trip = hotel['hotels_trip']
-            hotels_trip['trip_id'] = trip_id
+            hotel_id = insert_hotel(hotel)
+            print("Hotel ID:", hotel_id)
+            hotels_trip = hotel["hotels_trip"]
+            hotels_trip["trip_id"] = trip_id
             hotel_trip_id = insert_hotel_trip(hotels_trip)
-            print('Hotel Trip ID:', hotel_trip_id)
-
+            print("Hotel Trip ID:", hotel_trip_id)
 
         # for expense_trip in expenses_trip:
         #     expense_trip['trip_id'] = trip_id
@@ -37,13 +48,13 @@ for name, external_id in USERS.items():
 
         #         print('Expense ID:', expense_id)
 
-        for flight_trip in trip['voos']:
-            flight_trip['trip_id'] = trip_id
+        for flight_trip in trip["voos"]:
+            flight_trip["trip_id"] = trip_id
             flights_trip_id = insert_flight_trip(flight_trip)
 
-            print('Flight Trip ID:', flights_trip_id)
+            print("Flight Trip ID:", flights_trip_id)
 
-            flight = flight_trip['flight']
-            flight['flights_trip_id'] = flights_trip_id
+            flight = flight_trip["flight"]
+            flight["flights_trip_id"] = flights_trip_id
             flight_id = insert_flight(flight)
-            print('Flight ID:', flight_id)
+            print("Flight ID:", flight_id)
